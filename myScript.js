32e1b275d630497b636974adce14b103c668e44f
maggie <9thflr.maggie@gmail.com>
2017-10-13 18:42:04 +0800

    up

---
+++ myScript.js
@@ -0,0 +1,20 @@
+let banner = [
+  { story: "a如何瞭解蝴蝶蘭花苗產品的出口競爭和進口需求市場情報?", url: "powerbi_1.html" },
+  { story: "b如何瞭解蝴蝶蘭切花產品的出口競爭和進口需求市場情報?", url: "powerbi_2.html" },
+  { story: "c如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_3.html" },
+  { story: "d如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_4.html" },
+  { story: "e如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_5.html" }
+];
+// banner.forEach(function(e){
+//   console.log(e,e.story)  //e is the value
+// })
+
+let str =''
+
+
+// banner.forEach(function(e){
+//   str+="<div class="+'carousel-item'+'active'+" ><a href=" +e.url   +"  >"+e.story+"</a></div>"
+//   // str+="<div>"+e.story+"</div>"
+//   console.log(e.story)
+//   document.getElementById('test').innerHTML=str
+//   })
